3be8ae46b5a18bbc88906e462a5306411184f5c9
Eric Ha <ericha1981@gmail.com>
2019-02-02 16:08:14 -0600

    .gitignore created

--- app.js
+++ app.js
@@ -1,78 +1,79 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const bodyParser = require('body-parser');
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const bodyParser = require("body-parser");
 
 app.use(express.static(".")); // this solved the path problem in the html file.
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}));
+  })
+);
 app.use(bodyParser.json());
 
 // GET
-app.get('/', (req, res) => {
-    res.redirect('/login');
+app.get("/", (req, res) => {
+  res.redirect("/login");
 });
 
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/login.html');
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/login.html");
 });
 
-app.get('/flyroom', (req, res) => {
-    res.sendFile(__dirname + '/server.html');
+app.get("/flyroom", (req, res) => {
+  res.sendFile(__dirname + "/server.html");
 });
 
-const port = process.env.PORT || 5000;
+// Heroku port handling in production
+const PORT = process.env.PORT || 5000;
 
 var loggedUserIds = [];
 
-io.on('connection', (socket) => {
-    console.log('User is connected.');
+io.on("connection", socket => {
+  console.log("User is connected.");
 
-    const connectionCount = io.engine.clientsCount;
-    io.emit('loginCount', connectionCount);
+  const connectionCount = io.engine.clientsCount;
+  io.emit("loginCount", connectionCount);
 
-    
-    // 1. Display logged user name.
-    socket.on('logIn', (data, callback) => {
-        const logInMsg = `${data.name} joined the room.`;
-        socket.broadcast.emit('broadcast login', logInMsg);
+  // 1. Display logged user name.
+  socket.on("logIn", (data, callback) => {
+    const logInMsg = `${data.name} joined the room.`;
+    socket.broadcast.emit("broadcast login", logInMsg);
 
-        // Store users connected.
-        loggedUserIds.push(data);
-        callback(); // no parameter = no error. Callback function definition is in chattyfly-client.js
-    });
+    // Store users connected.
+    loggedUserIds.push(data);
+    callback(); // no parameter = no error. Callback function definition is in chattyfly-client.js
+  });
 
-    socket.on('logOut', (data) => { // this doesn't mean Socket is disconnected.
-        const logOutMsg = `${data.user} left the room.`;
+  socket.on("logOut", data => {
+    // this doesn't mean Socket is disconnected.
+    const logOutMsg = `${data.user} left the room.`;
 
-        // console.log(logOutMsg);
-        socket.broadcast.emit('broadcast logout', logOutMsg);
-    });
+    // console.log(logOutMsg);
+    socket.broadcast.emit("broadcast logout", logOutMsg);
+  });
 
-    // 2. Capture chat messages
-    socket.on('newchatfly', (data) => {
-        // sending to all clients except sender
-        socket.broadcast.emit('broadcast message', data);
-    });
+  // 2. Capture chat messages
+  socket.on("newchatfly", data => {
+    // sending to all clients except sender
+    socket.broadcast.emit("broadcast message", data);
+  });
 
-    // 3. Disconnect from the socket
-    socket.on('disconnect', () => {
-        console.log('User was disconnected.');
-        const disconnectedId = socket.id;
-        
-        // console.log(disconnectedId);
-        // console.log(loggedUserIds);
-        // console.log(loggedUserIds[0].id, loggedUserIds[0].name);
+  // 3. Disconnect from the socket
+  socket.on("disconnect", () => {
+    console.log("User was disconnected.");
+    const disconnectedId = socket.id;
 
+    // console.log(disconnectedId);
+    // console.log(loggedUserIds);
+    // console.log(loggedUserIds[0].id, loggedUserIds[0].name);
 
-        // Update the number of people online.
-        io.emit('loginCount', connectionCount);
-    });
+    // Update the number of people online.
+    io.emit("loginCount", connectionCount);
+  });
 });
 
-
-server.listen(port, () => {
-    console.log(`Server running on ${port}`);
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
